49d23c22971ed7d8b7c8e01c40e86a2b45332bf5
김재영 <kjyyy7341@gmail.com>
2024-07-06 19:53:39 +0900

    add res for app.use
    add res for app.use

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-app.use((req, next) => {
+app.use((req, res, next) => {
   let userAgent = req.headers['user-agent'] || '';
   if (userAgent.includes('Kakaotalk')) {
     req.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
