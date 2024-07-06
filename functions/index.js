e31713749d520c73c2c08e0a4fa4fb5978a8f17d
김재영 <kjyyy7341@gmail.com>
2024-07-06 19:50:07 +0900

    modify redirect process
    modify redirect process

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,14 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  const userAgent = req.headers['user-agent'] || '';
+app.use((req, next) => {
+  let userAgent = req.headers['user-agent'] || '';
   if (userAgent.includes('Kakaotalk')) {
-    res.redirect('https://pokedex-4d580.web.app/');
-  } else {
-    next();
+    req.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
   }
+  next();
 });
+
 
 app.use(express.static('public'));
 
